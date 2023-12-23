1204d616524aea09033f6cf4002d2041b0322145
IbnuSabil <ibnusabil2301@gmail.com>
2023-12-23 13:49:10 +0800

    style navbar

--- JS/main.js
+++ JS/main.js
@@ -34,9 +34,41 @@ const parentSkill = document.querySelector('.list-skill')
 
 var children = parentSkill.children
 var plusTimeDelay = 200
-console.log(children)
 for (var i = 0; i < children.length; i++) {
-    children[i].style.transitionDelay =  `${plusTimeDelay += 200}ms`
+    children[i].style.transitionDelay = `${plusTimeDelay += 200}ms`
 }
 
 // Animation Scrol
+
+const sections = document.querySelectorAll('section')
+const textNavbar = document.querySelectorAll('.txtNav')
+console.log(textNavbar)
+
+
+// Fungsi active section
+function updateActiveSection() {
+    const scrollPosition = window.scrollY;
+
+    sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            textNavbar[index].classList.add('active')
+        } else {
+            textNavbar[index].classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', updateActiveSection);
+
+updateActiveSection();
+
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
